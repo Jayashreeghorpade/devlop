c10d1b7d5e0bafc1b2c0dd173730cd9d4815ac28
Jayashreeghorpade <jayaghorpade1993@gmail.com>
2023-07-22 00:09:08 +0530

    yes

---
+++ imagehide.js
@@ -0,0 +1,108 @@
+var img1 = document.getElementById("item1")
+var img2 = document.getElementById("item2")
+var img3 = document.getElementById("item3")
+var img4 = document.getElementById("item4")
+var img5 = document.getElementById("item5")
+var img6 = document.getElementById("item6")
+var img7 = document.getElementById("item7")
+var img8 = document.getElementById("item8")
+var img9 = document.getElementById("item9")
+var img10 = document.getElementById("item10")
+var img11 = document.getElementById("item11")
+var img12 = document.getElementById("item12")
+var img13 = document.getElementById("item13")
+var img14 = document.getElementById("item14")
+var img15 = document.getElementById("item15")
+var img16 = document.getElementById("item16")
+function cofee(){
+    img1.style.display = "block"
+    img2.style.display = "block"
+    img3.style.display = "block"
+    img4.style.display = "block"
+    img5.style.display = "none"
+    img6.style.display = "none"
+    img7.style.display = "none"
+    img8.style.display = "none"
+    img9.style.display = "none"
+    img10.style.display = "none"
+    img11.style.display = "none"
+    img12.style.display = "none"
+    img13.style.display = "none"
+    img14.style.display = "none"
+    img15.style.display = "none"
+    img16.style.display = "none"
+    
+}
+function tea(){
+    img1.style.display = "none"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "block"
+    img6.style.display = "block"
+    img7.style.display = "block"
+    img8.style.display = "block"
+    img9.style.display = "none"
+    img10.style.display = "none"
+    img11.style.display = "none"
+    img12.style.display = "none"
+    img13.style.display = "none"
+    img14.style.display = "none"
+    img15.style.display = "none"
+    img16.style.display = "none"
+}
+function colddrink(){
+    img9.style.display = "block"
+    img10.style.display = "block"
+    img11.style.display = "block"
+    img12.style.display = "block"
+    img1.style.display = "none"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "none"
+    img6.style.display = "none"
+    img7.style.display = "none"
+    img8.style.display = "none"
+    img13.style.display = "none"
+    img14.style.display = "none"
+    img15.style.display = "none"
+    img16.style.display = "none"
+   
+}
+function fastfood(){
+    img13.style.display = "block"
+    img14.style.display = "block"
+    img15.style.display = "block"
+    img16.style.display = "block"
+    img1.style.display = "none"
+    img2.style.display = "none"
+    img3.style.display = "none"
+    img4.style.display = "none"
+    img5.style.display = "none"
+    img6.style.display = "none"
+    img7.style.display = "none"
+    img8.style.display = "none"
+    img9.style.display = "none"
+    img10.style.display = "none"
+    img11.style.display = "none"
+    img12.style.display = "none"
+}
+function allItem(){
+    img1.style.display = "block"
+    img2.style.display = "block"
+    img3.style.display = "block"
+    img4.style.display = "block"
+    img5.style.display = "block"
+    img6.style.display = "block"
+    img7.style.display = "block"
+    img8.style.display = "block"
+    img9.style.display = "block"
+    img10.style.display = "block"
+    img11.style.display = "block"
+    img12.style.display = "block"
+    img13.style.display = "block"
+    img14.style.display = "block"
+    img15.style.display = "block"
+    img16.style.display = "block"
+}
